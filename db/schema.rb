a20cbb5316cfd9f6a99ac24bc823e83561cbba60
tejasgowdaki <tejasgowda80@gmail.com>
2016-12-27 22:27:34 +0530

    name field added to user model' '

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227161003) do
+ActiveRecord::Schema.define(version: 20161227164141) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20161227161003) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
